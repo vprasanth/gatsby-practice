50fb4b1653c5c1132522ebffee5ff2f285ce8111
Prasanth Vaaheeswaran <prasanthv@icloud.com>
2018-03-02 08:54:22 -0500

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+export default () => <div style={{ color: 'tomato' }}>
+    <h1>Hello world!</h1>
+    <p>Hello Gatsby!</p>
+    <img src="https://source.unsplash.com/random/400x200" alt="" />
+  </div>
