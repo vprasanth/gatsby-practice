6df0361ac286628582507c25f2866715d8558294
Prasanth Vaaheeswaran <prasanthv@icloud.com>
2018-03-03 16:31:55 -0500

    Add buttons

--- src/pages/counter.js
+++ src/pages/counter.js
@@ -2,7 +2,14 @@ import React from "react";
 
 class Counter extends React.Component {
   render() {
-    return <div>Hello Class Compoonent</div>;
+    return (
+      <div>
+        <h1>Counter</h1>
+        <p>current count: 0</p>
+        <button>plus</button>
+        <button>minus</button>
+      </div>
+    );
   }
 }
 
