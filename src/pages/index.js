3b0df62c36c8abbd6ba84b649dc642993e5f94d8
Prasanth Vaaheeswaran <prasanthv@icloud.com>
2018-03-03 16:10:53 -0500

    Create page 2

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,12 @@
 import React from "react"
+import Link from "gatsby-link"
 
-export default () => <div style={{ color: 'tomato' }}>
+export default () => 
+  <div style={{ color: 'tomato' }}>
     <h1>Hello world!</h1>
     <p>Hello Gatsby!</p>
     <img src="https://source.unsplash.com/random/400x200" alt="" />
+    <div>
+      <Link to="page-2">Link</Link>
+    </div>
   </div>
